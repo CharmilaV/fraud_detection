a4cbd0e841b4b3c6529076aa8e9d0783fed684c5
CharmilaV <charmilav411@gmail.com>
2021-11-26 15:19:20 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,10 @@
 print("Adding all the data processing code related to fraud detection")
+
+print ("Trying commit without message")
+
+a = 5
+b = 7
+def sun(a,b):
+    c = a+b
+    return a
+print ("added new function sum which is incorrect")
