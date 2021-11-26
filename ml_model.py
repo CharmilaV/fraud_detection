0d345963347628ca6b46eecbbfab81e8f3566ba7
CharmilaV <charmilav411@gmail.com>
2021-11-26 15:57:45 +0530

    added 3 ml models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print ("aading all ML related code here")
+
+def model1():
+    print("adding code for ML model1")
+
+def model2():
+    print ("adding code for ML model2")
+
+def model2():
+    print ("adding code for ML model2")
